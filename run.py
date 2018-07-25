4f1a3699a5cb2569bee5bcd9b422afea82704f79
Daniel Cimring <daniel@blackhatmedia.com>
2018-07-25 11:48:51 -0500

    Supress warnings
    See https://stackoverflow.com/questions/40845304/runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ import bitmex
 import logging
 import logging.handlers
 import os
+import warnings
+
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 # unit tests eg for crossabove and crossunder
 # todo - cater for different strategies 
